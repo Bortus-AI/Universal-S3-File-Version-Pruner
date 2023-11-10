73c11e70c2a893822248519ffe24eccce23d0d29
Bortus-AI <100248124+Bortus-AI@users.noreply.github.com>
2023-11-10 12:00:10 -0600

    Update s3prune.sh
    revert accidental commit

--- s3prune.sh
+++ s3prune.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-set -e
-
 CONFIG_FILE="$HOME/.s3cleanupconfig"
 UNATTENDED=false
 DRY_RUN=true
-
-# Custom error function
-error_exit() {
-  echo "$1" >&2
-  exit 1
-}
 
 # Function to validate required parameters in unattended mode
 validate_required_parameters_unattended() {
@@ -22,16 +14,10 @@ validate_required_parameters_unattended() {
     missing_params+=("endpoint-url")
   fi
   if [ ${#missing_params[@]} -gt 0 ]; then
-    error_exit "Error: Missing required parameters in unattended mode: ${missing_params[*]}."
+    echo "Error: Missing required parameters in unattended mode: ${missing_params[*]}." >&2
+    exit 1
   fi
   MAX_KEYS=${MAX_KEYS:-1000} # Default MAX_KEYS if not provided
-}
-
-# Function to check for dependencies
-check_dependency() {
-  if ! command -v "$1" &>/dev/null; then
-    error_exit "Error: $1 is not installed. Please install it."
-  fi
 }
 
 # Parse command-line arguments
@@ -43,7 +29,7 @@ while [[ "$#" -gt 0 ]]; do
     --prefix) PREFIX="$2"; shift ;;
     --endpoint-url) ENDPOINT_URL="$2"; shift ;;
     --max-keys) MAX_KEYS="$2"; shift ;;
-    *) error_exit "Unknown parameter passed: $1"; exit 1 ;;
+    *) echo "Unknown parameter passed: $1"; exit 1 ;;
   esac
   shift
 done
@@ -58,23 +44,23 @@ else
   fi
   
   # Prompt for user input for each configuration item
-  echo "Please enter your configuration details:"
-  read -r -p "Bucket name (previous: $BUCKET): " input
+  read -r -p "Enter your bucket name [$BUCKET]: " input
   BUCKET=${input:-$BUCKET}
   
-  read -r -p "Prefix (leave empty for none, previous: $PREFIX): " input
+  read -r -p "Enter your prefix (or hit enter for none) [$PREFIX]: " input
   PREFIX=${input:-$PREFIX}
   
-  read -r -p "Endpoint URL (previous: $ENDPOINT_URL): " input
+  read -r -p "Enter your endpoint URL [$ENDPOINT_URL]: " input
   ENDPOINT_URL=${input:-$ENDPOINT_URL}
   
-  read -r -p "Max keys to delete per batch (default 1000, previous: $MAX_KEYS): " input
+  read -r -p "Enter max keys to delete per batch (default 1000) [$MAX_KEYS]: " input
   MAX_KEYS=${input:-${MAX_KEYS:-1000}}
   
   # Prompt for dry run mode with default value set to "y"
-  read -r -p "Enable dry run mode? (y/n, default: y): " DRY_RUN_INPUT
+  read -r -p "Enable dry run mode? (y/n) [y]: " DRY_RUN_INPUT
   DRY_RUN=$([[ -z "$DRY_RUN_INPUT" || "$DRY_RUN_INPUT" =~ ^[yY]$ ]] && echo true || echo false)
 fi
+
 
 # Normalize PREFIX to ensure it does not start with a slash and ends with one if not empty
 PREFIX="${PREFIX#/}"   # Remove leading slashes
@@ -92,24 +78,34 @@ if [ "$UNATTENDED" = false ]; then
     echo "MAX_KEYS=$MAX_KEYS"
     echo "DRY_RUN=$DRY_RUN"
   } > "$CONFIG_FILE"
-  echo "Configuration saved for future use."
 fi
 
+
 # Check for dependencies
-check_dependency "aws"
-check_dependency "jq"
+if ! command -v aws &>/dev/null; then
+  echo "Error: aws CLI is not installed. Please install it following the instructions at https://aws.amazon.com/cli/." >&2
+  exit 1
+fi
+
+if ! command -v jq &>/dev/null; then
+  echo "Error: jq is not installed. Please install it following the instructions at https://stedolan.github.io/jq/download/." >&2
+  exit 1
+fi
 
 # Validate configuration parameters
 if [ -z "$BUCKET" ]; then
-  error_exit "Error: Bucket name cannot be empty."
+  echo "Error: Bucket name cannot be empty." >&2
+  exit 1
 fi
 
 if ! [[ "$ENDPOINT_URL" =~ ^https?:// ]]; then
-  error_exit "Error: Invalid endpoint URL. It should start with http:// or https://."
+  echo "Error: Invalid endpoint URL. It should start with http:// or https://." >&2
+  exit 1
 fi
 
 if ! [[ "$MAX_KEYS" =~ ^[0-9]+$ ]]; then
-  error_exit "Error: Max keys should be a positive integer."
+  echo "Error: Max keys should be a positive integer." >&2
+  exit 1
 fi
 
 # Function to delete a batch of versions
@@ -134,11 +130,13 @@ delete_batch() {
 # Function to process deletion or dry run
 process_deletion() {
   if ! versions=$(aws s3api list-object-versions --bucket "$BUCKET" --prefix "$PREFIX" --endpoint-url "$ENDPOINT_URL"); then
-    error_exit "Error: Failed to list object versions."
+    echo "Error: Failed to list object versions." >&2
+    exit 1
   fi
 
   if ! versions_to_delete=$(echo "$versions" | jq -c '[.Versions[]? | select(.IsLatest | not) | {Key:.Key, VersionId:.VersionId}]'); then
-    error_exit "Error: Invalid JSON data from AWS or no versions available for prefix '$PREFIX'."
+    echo "Error: Invalid JSON data from AWS or no versions available for prefix '$PREFIX'." >&2
+    exit 1
   fi
 
   num_versions=$(echo "$versions_to_delete" | jq -r 'length')
